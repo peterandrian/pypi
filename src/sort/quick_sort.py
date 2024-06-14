5f4f77dfcd02e9748aa136ff927ae2fbdd6d08aa
peter <peterandrianpriestley@gmail.com>
2024-06-14 11:28:44 +0700

    tester updated

--- src/sort/quick_sort.py
+++ src/sort/quick_sort.py
@@ -1,20 +1,21 @@
-class quicksort:
-    def quicksort(listOfNumbers):
+class quick_sort:
+    def quick_sort(listOfNumbers):
         if len(listOfNumbers) <= 1:
             return listOfNumbers
         else:
-            pivot = [listOfNumbers[0]]
+            pivot = listOfNumbers[0]
         
         lessThanPivot = []
         moreThanPivot = []
 
         for number in listOfNumbers[1:]:
-            if number > pivot[0]:
+            if number > pivot:
                 moreThanPivot.append(number)
             else:
                 lessThanPivot.append(number)
         
-        return quicksort(lessThanPivot) + pivot + quicksort(moreThanPivot)
+        return quick_sort.quick_sort(lessThanPivot) + [pivot] + quick_sort.quick_sort(moreThanPivot)
 
-# result = quicksort([3, 12, 3, 4, 5, 61, 2, 4, 3, 8, 17, 53, 64, 3, 4, 6, 5, 8, 9])
+# Test the function
+# result = quick_sort.quick_sort([3, 12, 3, 4, 5, 61, 2, 4, 3, 8, 17, 53, 64, 3, 4, 6, 5, 8, 9])
 # print(result)
